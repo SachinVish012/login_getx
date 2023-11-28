ec4b9c940fbf9da391c6316196bdb2d6d54e646e
Sachin <sachinvishwakarma0122@gmail.com>
2023-11-28 09:55:57 +0530

    Routes Implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:login_getx/resources/routes/routes.dart';
 import 'package:login_getx/view/view_screen1.dart';
 
 void main() {
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeView(),
+       getPages: AppRoutes.appRoutes(),
     );
   }
 }
--- lib/view/view_screen1.dart
+++ lib/view/view_screen1.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
-
   @override
   State<HomeView> createState() => _HomeViewState();
 }
